259391f21026e076ae94ca39a1499e3dbf3c2354
chugue <chugue85@gmail.com>
2025-11-21 20:09:59 +0900

    savepoint: applicants type modified

--- src/server/domains/applicants/applicant.type.ts
+++ src/server/domains/applicants/applicant.type.ts
@@ -8,37 +8,10 @@ export type Applicant = {
   pdfDataHash: string;
   position: string; // 공개용 데이터
   techStack: string[]; // 공개용 데이터
-  careerDetail: string;
-  education: Education[];
-  experiences: Experience[];
-  introduction: string; // 공개용 데이터
-  projects: Project[]; // 수기 입력 필요
+  careerDetail: string; // 공개용 데이터
+  introduction: string; // 직접 입력
   accessPrice: number;
+  embedding: number[];
   isJobSeeking: boolean;
   accessList: Record<string, boolean>;
-};
-
-export type Education = {
-  school: string;
-  degree: string;
-  fieldOfStudy: string;
-  startDate: Date;
-  endDate: Date | null;
-  description: string;
-  isCurrentEducation: boolean;
-};
-
-export type Experience = {
-  companyName: string;
-  title: string;
-  startDate: Date;
-  endDate: Date | null;
-  description: string;
-  isCurrentJob: boolean;
-};
-
-export type Project = {
-  title: string;
-  url: string;
-  description: string;
 };
