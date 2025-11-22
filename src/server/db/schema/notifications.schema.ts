d017a02d936520087a628a3eeabfa56e2b39eb94
chugue <chugue85@gmail.com>
2025-11-22 22:01:41 +0900

    fix: db:migrate issue fixed

--- src/server/db/schema/notifications.schema.ts
+++
@@ -1,7 +0,0 @@
-import { pgTable } from "drizzle-orm/pg-core";
-
-export const notifications = pgTable("notifications", {});
-
-// Drizzle에서 타입 자동 유추
-export type Notification = typeof notifications.$inferSelect;
-export type NewNotification = typeof notifications.$inferInsert;
